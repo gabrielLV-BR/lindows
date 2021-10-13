4750337b9565bf9cd32c1250b9f876c968a991a2
Gabriel Lovato Vianna <gabriellovato15@gmail.com>
2021-10-13 14:57:39 -0300

    Importando projeto

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Hello <span class="special">{name}</span>!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style lang="scss">
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+
+		.special {
+			background-image: linear-gradient(
+				to right,
+				red, 
+				orange, 
+				yellow , 
+				green, 
+				blue
+			);
+
+			color: transparent;
+			background-clip: border-box;
+			-webkit-background-clip: text;
+		}
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
